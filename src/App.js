99c9b97e897e8b0dc152ee3b14cca4aa527ba932
Klippie <info@johanvanderklift.nl>
2022-09-29 20:59:37 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div>
+//       Schrijf hier jouw functie-component!
+//     </div>
+//   );
+// }
+//
+// export default App;
 
-function App() {
-  return (
-    <div>
-      Schrijf hier jouw functie-component!
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLightOn: true
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            isLightOn: !this.state.isLightOn,
+        });
+    }
+
+    render() {
+        return (
+            <button
+                type="button"
+                onClick={this.handleClick}
+            >
+                {this.state.isLightOn ? 'AAN' : 'UIT'}
+            </button>
+        );
+    }
 }
 
 export default App;
